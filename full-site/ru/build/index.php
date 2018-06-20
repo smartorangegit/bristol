f0a312890367775de34542fad58cbbd416e8f714
Sergey Khistovoy <sergeykhistovoy@gmail.com>
2018-06-20 12:12:05 +0300

    changed the deadline for putting into operation

--- full-site/ru/build/index.php
+++ full-site/ru/build/index.php
@@ -113,7 +113,7 @@
 
           </div>
 
-        <div class="nine_finish">Завершение строительства &minus; второй квартал 2018 года. </div>
+        <div class="nine_finish">Завершение строительства: II - III квартал 2018 года. </div>
         </div>
 
       </div>
--- full-site/ru/webcam/index.php
+++ full-site/ru/webcam/index.php
@@ -1,47 +1,36 @@
+<?php include_once('../../include/utm.php'); ?>
 <!DOCTYPE html>
 <html lang="ru">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ЖК BRISTOL Comfort House - вэб камера</title>
+    <title>≡ ЖК Бристоль 》 Веб камера жилого комплекса BRISTOL Comfort House</title>
+    <link rel="stylesheet" href="/css/intlTelInput.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/webcam.css">
     <link rel="stylesheet" href="/css/menu.css">
     <link rel="stylesheet" href="/css/animate.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script type="text/javascript" scr="/js/scrollissimo.min.js"></script>
-    <script type="text/javascript" src="/js/lib/greensock/TweenMax.min.js"></script>
-    <script type="text/javascript" src="/js/lib/scrollmagic/ScrollMagic.min.js"></script>
-    <script type="text/javascript" src="/js/lib/scrollmagic/plugins/animation.gsap.js"></script>
-    <script type="text/javascript" src="/js/lib/wow.min.js"></script>
-	<meta name="description" content="ЖК BRISTOL Comfort House вы можете посмотреть ход строительства онлайн Відділ продажу: ☎ (044) 22 77 144, Адреса м.Київ, впр-т В. Лобановського,146Б">
+	<meta name="description" content='【 Веб камера 】 BRISTOL Comfort House — это максимум преимуществ за разумную цену ... ✅【BRISTOL】5 минут пешком до: станции метро "Демиевская", школы, детского садика ....  ✅【BRISTOL】7 типов квартир, 25 этажей, подземный паркинг, закрытая придомовая терр... ✅【BRISTOL】пр-т В. Лобановского, 146-б'>
 	<link rel="canonical" href="<?php echo 'https://'.$_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI'];?>"/>
 	<link rel="alternate" hreflang="ru" href="https://bristol.house/ru/webcam/" />
 	<link rel="alternate" hreflang="uk" href="https://bristol.house/webcam/" />
   </head>
   <body>
-    <script>
-        var controller = new ScrollMagic.Controller();
-        new WOW().init();
-    </script>
     <?php include '../../include/menu_ru.php';?>
 <?php include '../../include/gtm.php';?>
     <div class="page_webcam section_ten">
-      <div class="ten_name bounceInLeft"><h1><img src="/img/icons/web_black.png" alt="web">Вэб камера</h1></div>
+      <div class="ten_name bounceInLeft"><h1><img src="/img/icons/web_black.png" alt="web">Веб камера</h1></div>
       <div class="webcam_box">
         <iframe width="100%" height="100%" src="https://macparts.kiev.ua:8403/player.html" frameborder="0" allowfullscreen></iframe>
 
 <!-- place for iframe -->
 
        </div>
-      <div class="nine_finish">Окончание строительства &minus; первый квартал 2018 года. </div>
+      <div class="nine_finish">Окончание строительства: второй-третий квартал 2018 года. </div>
     </div>
 
     <!-- </div> -->
-    <div class="footer">
-<?php include '../../include/futer_ru.php';?>
-    </div>
-    <script src="/js/script.js"></script>
-    <script type="text/javascript" src="/js/lib/wow.min.js"></script>
+    <?php include '../../include/futer_ru.php';?>
   </body>
 </html>
